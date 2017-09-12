324031b36d407b34792fa2e384b84e4744ced713
Harish247 <g.harish247@gmail.com>
2017-09-11 20:02:21 -0700

    init

--- designs.js
+++ designs.js
@@ -1,10 +1,12 @@
-// Select color input
-// Select size input
 
 // When size is submitted by the user, call makeGrid()
-
+const submitButton = document.getElementById("submit_button");
+submitButton.addEventListener("click",makeGrid);
 function makeGrid() {
-
-// Your code goes here!
-
+    let height = document.getElementById("input_height").value;
+    let width = document.getElementById("input_width").value;
+    let color = document.getElementById("colorPicker").value;
+    console.log(height);
+    console.log(width);
+    console.log(color);
 }
