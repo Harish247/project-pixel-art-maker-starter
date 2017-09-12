f0cf99f8a5d361f49418617753badde607a068dd
Harish247 <g.harish247@gmail.com>
2017-09-12 00:01:14 -0700

    adding grid, coloring grid

--- designs.js
+++ designs.js
@@ -1,12 +1,32 @@
 
 // When size is submitted by the user, call makeGrid()
-const submitButton = document.getElementById("submit_button");
-submitButton.addEventListener("click",makeGrid);
+const submitButton = $("#submit_button");
+let grid = $("#pixel_canvas");
+submitButton.click(makeGrid);
 function makeGrid() {
-    let height = document.getElementById("input_height").value;
-    let width = document.getElementById("input_width").value;
-    let color = document.getElementById("colorPicker").value;
-    console.log(height);
-    console.log(width);
-    console.log(color);
+    let height = $("#input_height").val();
+    let width = $("#input_width").val();
+    let color = $("#colorPicker").val();
+    let rows = $("tr");
+    rows.each(function(element){
+        this.remove();
+    });
+    for(let i=0;i<height;i++){
+        grid.append(`<tr id=${i}row></tr>`);
+        let gridRow = $(`#${i}row`);
+        for(let j=0;j<width;j++){
+            gridRow.append('<td></td');
+        }
+    }
+    $('td').each(function(element) {
+        $(this).click(function(){
+            if(this.checked){
+                $(this).css('background-color','#ffffff');
+                $(this).removeProp('checked');
+            }else{    
+                $(this).css('background-color',$("#colorPicker").val());
+                $(this).prop('checked',true);
+            };
+        })
+    });
 }
